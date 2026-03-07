a9d125e18bdcf84d7536900dfd8e6c13844ebd74
ariespinoza <a01645270@tec.mx>
2026-03-06 19:58:24 -0600

    Conexion BD

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+# cargar variables del .env
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+# conexión a PostgreSQL en Aiven
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
+from database import engine
 from routes.login_route import router as login_router
 
 app = FastAPI()
@@ -13,3 +15,13 @@ app.add_middleware(
 )
 
 app.include_router(login_router)  # ← así registras las rutas
+
+@app.get("/test-db")
+def test_db():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT NOW();"))
+            fecha = result.scalar()
+        return {"conexion": "exitosa", "hora_db": str(fecha)}
+    except Exception as e:
+        return {"error": str(e)}
