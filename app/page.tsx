b736fee880737a15cfdbef0bf6ca8a6094334b90
Santiago Cordova <A01029211@tec.mx>
2026-04-10 12:27:08 -0600

    LogIn solucionado con la pagina de dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,8 @@ import NewProject from "@/components/NewProject";
 import ProjectList from "@/components/ProjectList";
 import ChatBot from "@/components/ChatBot";
 import Documentacion from "@/components/Documentacion";
+import LoginScreen from "@/components/LogIn";
 
 export default function Home() {
-  const [expandDocs, setExpandDocs] = useState(false);
-  const [currentScreen, setCurrentScreen] = useState<"home" | "dashboard">("home");
-
-  return (
-    <main className="flex items-stretch gap-6 p-10 w-full h-screen overflow-hidden">
-      {currentScreen === "home" && (
-        <NewProject onEnterDashboard={() => setCurrentScreen("dashboard")} />
-      )}
-
-      {currentScreen === "dashboard" && (
-        <>
-          <ProjectList onGoHome={() => setCurrentScreen("home")} />
-
-      {!expandDocs && <ChatBot />}
-
-      <Documentacion
-        expanded={expandDocs}
-        onToggle={() => setExpandDocs(!expandDocs)}
-      />  
-
-
-          <Documentacion
-            expanded={expandDocs}
-            onToggle={() => setExpandDocs(!expandDocs)}
-          />
-        </>
-      )}
-    </main>
-  );
+  return <LoginScreen />;
 }
